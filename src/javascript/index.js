bb7c037a4af0afa783d68ad7015d50aa0a174243
Jaime-alv <jaime.alv.fdz@gmail.com>
2023-10-11 12:38:03 +0200

    Add canvas with default size

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,3 +1,32 @@
-const message = "Hello world";
-console.log(message);
+const gridContainer = document.querySelector(".grid-canvas");
+const CANVAS_SIZE = 500;
+function createGridItem(gridSize) {
+    console.log(gridSize);
+    let gridItemSize = `${gridSize}px`;
+    let gridItem = document.createElement("div");
+    gridItem.classList.add("grid");
+    gridItem.setAttribute("style", `width: ${gridItemSize}; height: ${gridItemSize};`);
+    gridItem.addEventListener("mouseenter", () => {
+        gridItem.style.backgroundColor = "black";
+    });
+    gridContainer.appendChild(gridItem);
+}
+function calculateGridItemSizePercentage(gridItems = 16) {
+    let pixels = (CANVAS_SIZE / gridItems);
+    return pixels;
+}
+function setCanvasSize() {
+    let canvasSize = `${CANVAS_SIZE}px`;
+    gridContainer.style.width = canvasSize;
+    gridContainer.style.height = canvasSize;
+}
+function displayGridItems(gridCount = 16) {
+    const gridItemSize = calculateGridItemSizePercentage(gridCount);
+    const totalGridItem = gridCount * gridCount;
+    for (let i = 1; i < totalGridItem + 1; i++) {
+        createGridItem(gridItemSize);
+    }
+}
+setCanvasSize();
+displayGridItems(16);
 //# sourceMappingURL=index.js.map
--- src/typescript/index.ts
+++ src/typescript/index.ts
@@ -1,3 +1,36 @@
-const message = "Hello world";
+const gridContainer: HTMLElement = document.querySelector(".grid-canvas")!;
+const CANVAS_SIZE: number = 500;
 
-console.log(message)
+function createGridItem(gridSize: number) {
+    console.log(gridSize);
+    let gridItemSize: string = `${gridSize}px`;
+    let gridItem = document.createElement("div");
+    gridItem.classList.add("grid");
+    gridItem.setAttribute("style", `width: ${gridItemSize}; height: ${gridItemSize};`);
+    gridItem.addEventListener("mouseenter", () => {
+        gridItem.style.backgroundColor = "black";
+    })
+    gridContainer.appendChild(gridItem);
+}
+
+function calculateGridItemSizePercentage(gridItems: number = 16): number {
+    let pixels: number = (CANVAS_SIZE / gridItems);
+    return pixels
+}
+
+function setCanvasSize() {
+    let canvasSize = `${CANVAS_SIZE}px`;
+    gridContainer.style.width = canvasSize;
+    gridContainer.style.height = canvasSize;
+}
+
+function displayGridItems(gridCount: number = 16) {
+    const gridItemSize: number = calculateGridItemSizePercentage(gridCount);
+    const totalGridItem: number = gridCount * gridCount;
+    for (let i = 1; i < totalGridItem + 1; i++) {
+        createGridItem(gridItemSize);
+    }
+}
+
+setCanvasSize();
+displayGridItems(16);
